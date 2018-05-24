44d6df2fa8588caf010bfad19bfc7e0a9f703257
textstorm <jiangzhewise@gmail.com>
2018-05-24 08:40:46 +0800

    update

--- CelebA/utils.py
+++ CelebA/utils.py
@@ -1,8 +1,8 @@
 
 import tensorflow as tf
 import collections
-
-Datasets = collections.namedtuple('Datasets', ['train', 'test'])
+import scipy.misc
+import numpy as np
 
 def preprocess_fn(img):
   crop_size = 108
@@ -49,7 +49,7 @@ class DiskImageData:
   def __len__(self):
     return self._data_num
 
-  def batch(self):
+  def next_batch(self):
     return self.sess.run(self._batch_ops)
 
   def __del__(self):
@@ -68,3 +68,40 @@ def get_config_proto(log_device_placement=False, allow_soft_placement=True):
       allow_soft_placement=allow_soft_placement)
   config_proto.gpu_options.allow_growth = True
   return config_proto
+
+def to_range(images, min_value=0.0, max_value=1.0, dtype=None):
+  assert \
+    np.min(images) >= -1.0 - 1e-5 and np.max(images) <= 1.0 + 1e-5 \
+    and (images.dtype == np.float32 or images.dtype == np.float64), \
+    'The input images should be float64(32) and in the range of [-1.0, 1.0]!'
+  if dtype is None:
+    dtype = images.dtype
+  return ((images + 1.) / 2. * (max_value - min_value) + min_value).astype(dtype)
+
+def save_images(images, size, image_path):
+  """Save mutiple images into one single image.
+  Parameters
+  -----------
+  images : numpy array [batch, w, h, c]
+  size : list of two int, row and column number.
+      number of images should be equal or less than size[0] * size[1]
+  image_path : string.
+  Examples
+  ---------
+  >>> images = np.random.rand(64, 100, 100, 3)
+  >>> utils.save_images(images, [8, 8], 'temp.png')
+  """
+  def merge(images, size):
+    h, w = images.shape[1], images.shape[2]
+    img = np.zeros((h * size[0], w * size[1], 3))
+    for idx, image in enumerate(images):
+      i = idx % size[1]
+      j = idx // size[1]
+      img[j*h:j*h+h, i*w:i*w+w, :] = image
+    return img
+
+  def imsave(images, size, path):
+    return scipy.misc.imsave(path, to_range(merge(images, size), 0, 255, np.uint8))
+
+  assert len(images) <= size[0] * size[1], "number of images should be equal or less than size[0] * size[1] {}".format(len(images))
+  return imsave(images, size, image_path)
